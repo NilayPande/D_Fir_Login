1878a909f017b1f18c34759b577319f69a04b19b
HrithikSharma <hrithik.a.sharma.28@gmail.com>
2020-08-01 22:15:43 +0530

    FinalGradle
    Login and Registration

---
+++ DFIR_LoginAndRegistration/app/src/main/java/com/example/d_fir_login/Model/User.java
@@ -0,0 +1,62 @@
+package com.example.d_fir_login.Model;
+
+public class User {
+
+    private String Name;
+    private String EmployeeId;
+    private String PhoneNumber;
+    private String Password;
+    private String Designation;
+
+    public User(){
+
+    }
+
+    public User(String name, String employeeId, String phoneNumber, String password, String designation) {
+        Name = name;
+        EmployeeId = employeeId;
+        PhoneNumber = phoneNumber;
+        Password = password;
+        Designation = designation;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        EmployeeId = employeeId;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getDesignation() {
+        return Designation;
+    }
+
+    public void setDesignation(String designation) {
+        Designation = designation;
+    }
+}
